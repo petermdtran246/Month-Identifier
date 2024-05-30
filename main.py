ba02a0af7fc8ba9946f1e2cfc2668e9c3a4a2405
petermdtran246 <petermdtran246@gmail.com>
2024-05-30 22:40:41 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def MonthName(m):
+    if m == 1:
+        print('jan')
+    elif m == 2:
+        print('feb')
+    elif m == 3:
+        print('mar')
+    elif m == 4:
+        print('apr')
+    elif m == 5:
+        print('may')
+    elif m == 6:
+        print('jun')
+    elif m == 7:
+        print('jul')
+    elif m == 8:
+        print('aug')
+    elif m == 9:
+        print('sep')
+    elif m == 10:
+        print('oct')
+    elif m == 11:
+        print('nov')
+    elif m == 12:
+        print('dec')
+
+m = int(input('Enter Month number: '))
+MonthName(m)
